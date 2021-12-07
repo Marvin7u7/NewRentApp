de914e66d6aa3d5d693a83358983984815abd266
jxareas <areasjx@gmail.com>
2021-12-07 01:47:24 -0600

    Add Splash Screen

---
+++ app/src/main/java/com/jonareas/rentapp/splash/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.jonareas.rentapp.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.jonareas.rentapp.MainActivity
+import com.jonareas.rentapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val TIME_DELAY_IN_MILIS = 1000.toLong()
+
+        Handler(Looper.myLooper()!!).postDelayed(
+            {
+                startFirstActivity()
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                finish()
+            }, TIME_DELAY_IN_MILIS
+        )
+    }
+
+    private fun startFirstActivity() = startActivity(Intent(this, MainActivity::class.java))
+
+
+
+
+
+
+
+}

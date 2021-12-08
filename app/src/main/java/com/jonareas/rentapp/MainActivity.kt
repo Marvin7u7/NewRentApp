e5d1542c6bc16add2925dc34e61c8ffafba62f6d
jxareas <areasjx@gmail.com>
2021-12-08 11:40:58 -0600

    Navigation Example

--- app/src/main/java/com/jonareas/rentapp/MainActivity.kt
+++ app/src/main/java/com/jonareas/rentapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.jonareas.rentapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.jonareas.rentapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentLandlord) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.add_house, R.id.edit_house, R.id.edit_profile, R.id.settings))
+
+        setupActionBarWithNavController(navController = navController, configuration = appBarConfiguration)
+
+        binding.bottomNavigation.setupWithNavController(navController)
+
+
     }
 }
